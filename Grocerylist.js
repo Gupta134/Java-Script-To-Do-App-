7b7b9f175126ff15fbb761ec80d3a507ff9c920c
Gaurav gupta <115206859+Gupta134@users.noreply.github.com>
2023-06-19 15:10:13 +0530

    NA

---
+++ Grocerylist.js
@@ -0,0 +1,32 @@
+$(function() {
+	var grocery = function(event) {
+		event.preventDefault();
+		var input = $('input[type=text]'),
+			value = input.val(),
+			need = ($(event.target).attr('id') === 'addNeed'),
+			item = $('<li><input type="checkbox" name="item"> ' + value + ' <a href="#">remove</a></li>'),
+			list = (need) ? $('ul').first() : $('ul').last();
+		
+		input.val("");
+		input.focus();
+
+		if (value === "") return;
+
+		if (!need) {
+			item.find('input').attr('checked', true);
+		}
+		item.appendTo(list);
+	}
+
+	$('#addHave, #addNeed').click(grocery);
+	
+	$('ul').on('click', 'li a', function(event){
+		$(event.target).parent('li').remove();
+	});
+
+	$('ul').on('click', 'input[type=checkbox]', function(event){
+		var listItem = $(event.target).parent('li'),
+			list = (event.target.checked) ? $('ul').last() : $('ul').first();
+		listItem.appendTo(list);
+	});
+});
